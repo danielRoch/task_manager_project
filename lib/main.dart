16c072ee97c11a04b569da260153dc5b6bb86cde
Emioluwa Adenuga <72270452+emiadenuga@users.noreply.github.com>
2023-07-14 22:22:08 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task Manager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
